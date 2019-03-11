9b2b48b63c204e4f0c7327ee8d04476f70bd8d9a
rafabrunof <44882072+rafabrunof@users.noreply.github.com>
2019-03-11 15:33:22 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    /* INPUT FORMULARIO*/
+    $('#input').characterCounter();
+
+    /* SCROLLSPY*/
+    $('.scrollspy').scrollSpy();
+
+    /* BUTTON FLOATING */
+    $('.fixed-action-btn').floatingActionButton();
+
+    /* SIDENAV */
+    $('.sidenav').sidenav();
+
+});
